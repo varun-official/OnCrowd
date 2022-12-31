280fcb02f2a40bdcef238f7b3e8a0ae2c4a2c330
Varun <varunvadda99@gmail.com>
2022-12-31 14:32:09 +0530

    Created folder for client codes

---
+++ client/src/pages/index.js
@@ -0,0 +1,6 @@
+/** @format */
+
+export { default as Home } from "./Home";
+export { default as Profile } from "./Profile";
+export { default as Campaign } from "./Campaign";
+export { default as CreateCampaign } from "./CreateCampaign";
