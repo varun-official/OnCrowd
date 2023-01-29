90ee8dea73d75be4dc677f8c7faf847552e14ad2
Varun <varunvadda99@gmail.com>
2023-01-29 23:43:37 +0530

    Workong on context provider for smart contract

---
+++ client/src/context/index.jsx
@@ -0,0 +1,55 @@
+/** @format */
+
+import React, { useContext, createContext } from "react";
+import {
+  useAddress,
+  useContract,
+  useMetamask,
+  useContractWrite,
+} from "@thirdweb-dev/react";
+import { ethers } from "ethers";
+
+const StateContext = createContext();
+
+export const StateContextProvider = ({ children }) => {
+  const { contract } = useContract(
+    "0x3804b9aFC4C201b6c19e379e556c416ee9Ca3a61"
+  );
+  const { mutateAsync: createCampaign } = useContractWrite(
+    contract,
+    "createCampaign"
+  );
+
+  const address = useAddress();
+  const connect = useMetamask();
+
+  const publishCampaign = async (form) => {
+    try {
+      const data = await createCampaign([
+        address, //owner
+        form.title, //title
+        form.description, //description
+        form.target, //target
+        new Date(form.deadline).getTime(), //deadline
+        form.image, //image
+      ]);
+      console.log("Contract call success", data);
+    } catch (error) {
+      console.log("publishCampaign:" + error);
+    }
+  };
+
+  return (
+    <StateContext.Provider
+      value={{
+        address,
+        contract,
+        createCampaign: publishCampaign,
+      }}
+    >
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(StateContext);
