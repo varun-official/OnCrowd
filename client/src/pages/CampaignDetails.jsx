b8f0df42861b0ff93a3a75d151dcbfb64436827f
Varun <varunvadda99@gmail.com>
2023-02-15 00:07:43 +0530

    Working on finalpage

--- client/src/pages/CampaignDetails.jsx
+++ client/src/pages/CampaignDetails.jsx
@@ -52,7 +52,7 @@ const CampaignDetails = () => {
       <div className="mt-[60px] flex lg:flex-row flex-col gap-5">
         <div className="flex-[2] flex flex-col gap-[40px]">
           <div>
-            <h4 className="font-epilogue font-semibold text-[18px] text-white p-3 uppercase">
+            <h4 className="font-epilogue font-semibold text-[18px] text-white uppercase">
               Creator
             </h4>
             <div className="mt-[20px] flex flex-row items-center flex-wrap gap-[14px]">
@@ -75,33 +75,40 @@ const CampaignDetails = () => {
           </div>
 
           <div>
-            <h4 className="font-epilogue font-semibold text-[18px] text-white p-3 uppercase">
+            <h4 className="font-epilogue font-semibold text-[18px] text-white uppercase">
               Story
             </h4>
+            <div className="mt-[20px]">
+              <p className="font-epilogue font-normal text-[16px] text-[#808191] leading-[26px] text-justify">
+                {state.description}
+              </p>
+            </div>
           </div>
 
           <div>
-            <h4 className="font-epilogue font-semibold text-[18px] text-white p-3 uppercase">
-              Creator
+            <h4 className="font-epilogue font-semibold text-[18px] text-white uppercase">
+              Donators
             </h4>
-            <div className="mt-[20px] flex flex-row items-center flex-wrap gap-[14px]">
-              <div className="w-[52px] h-[52px] flex item-center justify-center rounded-full bg-[#2c2f32] cursor-pointer">
-                <img
-                  src={thirdweb}
-                  alt="user"
-                  className="w-[60%] h-[60%] object-contain"
-                />
-              </div>
-              <div>
-                <h4 className="font-epilogue font-semibold text-[14px] text-white break-all">
-                  {state.owner}
-                </h4>
-                <p className="mt-[4px] font-epilogue font-normal text-[12px] text-[#808191]">
-                  10 Campaigns
+            <div className="mt-[20px] flex flex-col gap-4">
+              {donators.length > 0 ? (
+                donators.map((donator, index) => <div>Donator</div>)
+              ) : (
+                <p className="font-epilogue font-normal text-[16px] text-[#808191] leading-[26px] text-justify">
+                  No Donators you be the first one.
                 </p>
-              </div>
+              )}
             </div>
           </div>
+        </div>
+        <div className=" flex-1">
+          <h4 className="font-epilogue font-semibold text-[18px] text-white uppercase">
+            Fund
+            <div className="mt-[20px] flex flex-col p-4 bg-[#1c1c24] rounded-[10px]">
+              <p className="font-epilogue font-medium text-[20px] leading-[30px] text-center text-[#808191]">
+                Fund the Campaign
+              </p>
+            </div>
+          </h4>
         </div>
       </div>
     </div>
